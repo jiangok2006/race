5bd8b3c1b12c1bb2b77a7fb96746ea3a64dca797
Lian Jiang <lian.jiang@opendoor.com>
2023-11-25 20:04:04 -0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@ npm install
 npx playwright install --with-deps
 
 # $1: preview|production, $2: dev.race-dui.pages.dev|race-dui.pages.dev
-export APP_HTTP_URL=$2
+export APP_HTTP_URL="http://$2"
 npm run test --dir ./tests/monitoring
