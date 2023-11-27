9fce9d3e73b64affbef3ca5ef43feb8732f5525d
Lian Jiang <lian.jiang@opendoor.com>
2023-11-26 19:24:04 -0800

    use playwright for e2e test instead of vitest

---
+++ tests/e2e/signup_signin.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test';
+import { v4 as uuidv4 } from 'uuid';
+
+test('signup and signin', async ({ page }) => {
+  let email = 'test-' + uuidv4() + '@test.com'
+  let password = process.env.TEST_USER_PASSWORD!
+  await page.goto('http://127.0.0.1:8788/');
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.getByPlaceholder('name@example.com').click();
+  await page.getByPlaceholder('name@example.com').fill(email);
+  await page.getByLabel('Password', { exact: true }).click();
+  await page.getByLabel('Password', { exact: true }).fill(password);
+  await page.getByLabel('Confirm Password').click();
+  await page.getByLabel('Confirm Password').fill(password);
+  await page.getByRole('button', { name: 'Sign Up with Email' }).click();
+  await page.waitForLoadState('networkidle');
+  await page.locator("#email").click();
+  await page.locator("#email").fill(email);
+  await page.locator("#password").click();
+  await page.locator("#password").fill(password)
+  await page.getByRole('button', { name: 'Sign In with Email' }).click();
+  await page.waitForLoadState('networkidle');
+  await expect(page.getByLabel('Select a team')).toBeVisible();
+});
+
