7db935e0b1c24bd0e840324e44d0783adadc68b4
Lian Jiang <lian.jiang@opendoor.com>
2023-11-27 03:50:03 -0800

    add unit, int test examples

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 npm install
 npx playwright install --with-deps
 
-# $1: monitoring|e2e|integration|unit
-# $2: preview|production
-# $3: dev.race-dui.pages.dev|race-dui.pages.dev
+# $1: integration|unit
+# $2: preview
+# $3: dev.race-dui.pages.dev
 export APP_HTTP_URL="http://$3"
 npm run test --dir "./tests/$1"
